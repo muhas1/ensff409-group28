ffe7dc554a8d0e78e995a1f514db45a9e266d5f9
ArushSanghal <101238461+ArushSanghal@users.noreply.github.com>
2022-04-15 20:39:14 -0600

    Added comments to code

--- edu/ucalgary/ensf409/textFormat.java
+++ edu/ucalgary/ensf409/textFormat.java
@@ -1,6 +1,8 @@
 package edu.ucalgary.ensf409;
 
-public class textFormat {
+public class textFormat {       //creates an interface which will be implemented in order form class
+  
+  String fmtString(); 
 
 }
 
