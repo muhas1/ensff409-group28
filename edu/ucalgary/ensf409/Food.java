92914494bd6c3fc04d72aa4d081bf9059b4ad93a
ArushSanghal <101238461+ArushSanghal@users.noreply.github.com>
2022-04-18 20:55:32 -0600

    Added comments

--- edu/ucalgary/ensf409/Food.java
+++ edu/ucalgary/ensf409/Food.java
@@ -1,45 +1,45 @@
-package edu.ucalgary.ensf409;
-
 public class Food {
+	
+	//Used in order to find and store the nutritional values of different foods in the inventory for later usage.
     
-    private String[] FOODINFO = new String[2];
+    private String[] FOODINFO = new String[2];			//initialize variables
     private NutritionInfo NUTR_INFO = new NutritionInfo("0", "0", "0", "0", "0");
 
-    public Food(String[] info, NutritionInfo nInfo) {
+    public Food(String[] info, NutritionInfo nInfo) {		//constructors for Food
         this.FOODINFO = info;
         this.NUTR_INFO = nInfo;
     }
 
-    public String[] getFoodInfo() {
+    public String[] getFoodInfo() {					//returns FoodInfo
         return this.FOODINFO;
     }
 
-    public NutritionInfo getNutritionInfo() {
+    public NutritionInfo getNutritionInfo() {		//returns NutritionInfo
         return this.NUTR_INFO;
     }
 
-    public String getFoodID() {
+    public String getFoodID() {				//returns FoodID
         return this.FOODINFO[0];
     }
 
-    public String getFoodName() {
+    public String getFoodName() {			//returns FoodName
         return this.FOODINFO[1];
     }
 
-    public double getFruitsVeggies() {
+    public double getFruitsVeggies() {		//returns FruitsVeggies calories
         return this.NUTR_INFO.getFV();
     }
 
     public double getCalories() {
-        return this.NUTR_INFO.getCalories();
+        return this.NUTR_INFO.getCalories();	//returns calories
     }
     
     public double getProtein() {
-        return this.NUTR_INFO.getProtein();
+        return this.NUTR_INFO.getProtein();		//returns protein calories
     }
     
     public double getOther() {
-        return this.NUTR_INFO.getOther();
+        return this.NUTR_INFO.getOther();		//returns other calories
     }
 
     public double getGrains() {
